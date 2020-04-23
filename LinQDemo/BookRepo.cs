e11ba9e816963694dfcc97e13b877985c37c5941
Your Name <you@example.com>
2020-04-23 02:54:53 -0400

    initial commit

---
+++ LinQDemo/BookRepo.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LinQDemo
+{
+    public class BookRepo
+    {
+        public IEnumerable<Book> GetBooks() {
+            return new List<Book>
+            {
+                new Book(){ Title = "ASP.net", Price = 10},
+                new Book(){ Title = "XML beginner", Price = 5 },
+                new Book(){ Title = "C#.net", Price = 8 },
+                new Book(){ Title = "ASP.net", Price = 7 },
+                new Book(){ Title = "Java Enterprise", Price = 10 }
+                
+            };
+        }
+    }
+}
---
+++ LinQDemo/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Linq;
+
+namespace LinQDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var books = new BookRepo().GetBooks();
+            //var cheapbooks = from b in books
+            //                 where b.Price < 10
+            //                 orderby b.Title
+            //                 select b;
+            //var cheapbooks = books
+            //                    .Where(b => b.Price < 10)
+            //                    .OrderBy(b=>b.Title)
+            //                    .Select(b=>b.Title);
+
+            //foreach(var book in cheapbooks)
+            //   //Console.WriteLine(book.Title+" "+book.Price);
+            //   Console.WriteLine(book.Title);
+            //var book = books.SingleOrDefault(b => b.Title == "ASP.NET MVC++");
+            //var book = books.Single(b => b.Title == "ASP.NET MVC++");
+            //var maxPrice = books.Max(b => b.Price);
+            //var minPrice = books.Min(b => b.Price);
+            //var avg = books.Average(b => b.Price);
+            int a = 5, b = 4;
+            Action action = () => Console.WriteLine(a * b);
+            action();
+            Func<int, int> f  = b => b * b;
+            //Console.WriteLine(maxPrice +" "+ minPrice +" "+ avg);
+            Console.WriteLine(f(2));
+
+            Action<int> ac = b =>Console.WriteLine( b * b);
+            //Console.WriteLine(maxPrice +" "+ minPrice +" "+ avg);
+            Console.WriteLine(f(2));
+
+        }
+    }
+}
